565a68ab07e27147265402709af54f7a32b6264b
AtsushiNorimura <anorimura+dev@gmail.com>
2025-05-04 12:53:42 +0900

    GitHub Page用設定

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // 必要に応じて他の設定を追加
+  // GitHub Pagesのため、リポジトリ名がURL一部になる場合の設定
+  // basePath: '/miyoshino-site', // リポジトリ名をここに指定（必要な場合のみコメントを外す）
+  // GitHub Pagesでの画像読み込みのための設定
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
